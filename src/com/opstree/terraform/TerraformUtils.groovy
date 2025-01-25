3fc604ae858baf3eda08a7065ebdd0a5a20abcdf
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2025-01-25 19:21:11 +0530

    Update TerraformUtils.groovy

--- src/com/opstree/terraform/TerraformUtils.groovy
+++ src/com/opstree/terraform/TerraformUtils.groovy
@@ -1,1 +1,30 @@
+package com.opstree.terraform
 
+class TerraformUtils {
+    static def validateEnvironment(config) {
+        def requiredParams = ['environment', 'stateBucket', 'stateKey', 'region']
+        
+        requiredParams.each { param ->
+            if (!config[param]) {
+                throw new Exception("Missing required parameter: ${param}")
+            }
+        }
+    }
+
+    static def generateTags(config) {
+        def defaultTags = [
+            'ManagedBy': 'Terraform',
+            'Environment': config.environment,
+            'Organization': 'Opstree'
+        ]
+
+        def customTags = config.customTags ?: [:]
+        return defaultTags + customTags
+    }
+
+    static def sanitizeInputs(config) {
+        config.terraformVersion = config.terraformVersion ?: '1.5.7'
+        config.project = config.project ?: 'Unknown'
+        return config
+    }
+}
