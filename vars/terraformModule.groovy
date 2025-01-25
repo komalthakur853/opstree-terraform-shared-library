08c67e1e710a7d910eb5f273daa07a9c7d1dfa4e
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2025-01-25 19:22:01 +0530

    Update terraformModule.groovy

--- vars/terraformModule.groovy
+++ vars/terraformModule.groovy
@@ -1,4 +1,11 @@
+import com.opstree.terraform.TerraformUtils
+
 def call(Map config = [:]) {
+    // Validate and sanitize inputs
+    TerraformUtils.validateEnvironment(config)
+    config = TerraformUtils.sanitizeInputs(config)
+
+    // Create and execute pipeline
     def pipeline = new com.opstree.terraform.TerraformPipeline(this, config)
     pipeline.execute()
 }
